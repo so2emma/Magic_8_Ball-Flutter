7ba914db39774c06bc1154431460a00af6d5abd3
so2emma <osoemmanuel1969@gmail.com>
2024-01-02 21:07:46 +0100

    imported image to the view page

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,35 @@ class BallPage extends StatelessWidget {
       backgroundColor: Colors.blue,
       appBar: AppBar(
         backgroundColor: Colors.blue[800],
-        title: Text('Ask me anything',
-        style: TextStyle(color: Colors.white),),
+        title: Text(
+          'Ask me anything',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
       ),
+      body: Ball(),
+    );
+  }
+}
 
-      body: Container(),
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Expanded(
+          child: Image(
+            image: AssetImage('images/ball1.png'),
+          ),
+        ),
+      ),
     );
   }
 }
