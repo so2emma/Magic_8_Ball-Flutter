df42cf93c7d5219321cd3d81400b7a5aa397e33b
so2emma <osoemmanuel1969@gmail.com>
2024-01-02 20:43:42 +0100

    initialization file

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp();
+}
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({super.key});
+
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
